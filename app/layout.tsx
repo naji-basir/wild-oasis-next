f7c3597c8b64a4a7d81f72a9ee261a636fec96cc
Basir Naji <basirnaji007@gmail.com>
2026-02-12 02:48:53 +0430

    metadate + title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import Navigations from "./components/Navigations";
 import { ReactNode } from "react";
 
+export const metadata = { title: "The Wild Oasis" };
+
 function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html>
+    <html lang="en">
       <body>
         <Navigations />
         <main>{children}</main>
