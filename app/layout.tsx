ff4811681602528cf460fe7d2fd350519170399b
Basir Naji <basirnaji007@gmail.com>
2026-02-12 03:01:02 +0430

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Navigations from "./components/Navigations";
 import { ReactNode } from "react";
+import Logo from "./components/Logo";
 
 export const metadata = { title: "The Wild Oasis" };
 
@@ -7,8 +8,14 @@ function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Navigations />
+        <header>
+          <Logo />
+          <Navigations />
+        </header>
         <main>{children}</main>
+        <footer>
+          <p>Basir</p>
+        </footer>
       </body>
     </html>
   );
