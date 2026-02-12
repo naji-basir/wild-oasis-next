a89f7eb7cdba094518bae427af9bc8c84dbf1c9a
Basir Naji <basirnaji007@gmail.com>
2026-02-12 15:00:12 +0430

    SRC

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,16 @@
-function Page() {
+async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+
   return (
     <div>
       <h1 className=" bg-amber-200">CABINS</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>{" "}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Counter from "./components/Counter";
 function Page() {
   return (
     <div>
       <h1 className="bg-emerald-300">THE WILD OASIS WITH NEXT❤️</h1>
-      <Link href="/cabins">cabins</Link>
+      <Counter />
     </div>
   );
 }
